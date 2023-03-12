7369f560706d84e642982e99749ebbc94767be1d
HCCuong <102528939+HCCuong@users.noreply.github.com>
2023-03-12 20:23:44 +0700

    Create course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Course = new Schema({
-  name: { type: String, maxLength: 600 },
-  description: { type: String, maxLength: 600 },
+  name: { type: String, required: true },
+  description: { type: String },
   image: { type: String, maxLength: 600 },
-  createdAt: {type: Date, default: Date.now },
-  updatedAt: {type: Date, default: Date.now }
+  videoId: { type: String, required: true },
+  level: { type: String, maxLength: 255 },
+  slug: { type: String, slug: 'name', unique: true }
+}, {
+  timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
